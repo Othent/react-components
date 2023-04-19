701bd04c330e0725c4f141eaecafbd7959dcf0ce
7i7o <mparij@gmail.com>
2023-04-19 09:21:49 -0300

    add logout call to othent

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -1,24 +1,38 @@
-import React, { HTMLProps } from "react";
+import React, { useState } from "react";
 import "./LogoutButton.css";
+import othent from "othent/dist/lib";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode | string;
   className?: string;
+  onLogout?: () => void;
 }
 
 const Button = (props: ButtonProps) => {
-  const { children = "Log Out", className = "" } = props;
+  const { children = "Log Out", className = "", onLogout } = props;
 
-  const logout = () => {
-    // TODO: Call othent.logOut()
-    console.log("TODO: call othent.LogOut()");
+  const [clicked, setClicked] = useState(false);
+
+  const logout = async () => {
+    setClicked(true);
+    try {
+      const logoutResponse = await othent.logOut();
+      console.log(logoutResponse);
+      if (onLogout) onLogout();
+    } catch (e) {
+      console.log(`othent.logout() failed:`);
+      console.log(e);
+    } finally {
+      setClicked(false);
+    }
   };
 
   return (
     <button
       className={`othent-button-logout ${className}`}
-      onClick={logout}
+      disabled={clicked}
+      onClick={() => void logout()}
       {...props}
     >
       {children}
